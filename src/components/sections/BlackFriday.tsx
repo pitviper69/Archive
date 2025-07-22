1441db63928144a4304d1caf058084e27a0a9425
Mikayla Howitz <mikaylahowitz@mac.lan>
2025-07-22 11:31:43 -0600

    fix bg image, change title, add favicon

--- src/components/sections/BlackFriday.tsx
+++ src/components/sections/BlackFriday.tsx
@@ -7,7 +7,7 @@ import FAQ  from "./FAQ.tsx";
 export const BlackFriday = () => {
     return (
        <>
-           <div className="bg-[url(../../assets/tiling-background-1080.jpg)] bg-repeat bg-contain pt-40">
+           <div className="bg-[url(./assets/tiling-background-1080.jpg)] bg-repeat bg-contain pt-40">
                <BlackFridayHeader />
                <BlackFridayIntro/>
                <PreviousPrizes/>
